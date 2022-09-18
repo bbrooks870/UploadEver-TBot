248f59954fd3bb40644680e6913cd3e01b64e049
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-19 00:31:39 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command("start") & filters.private)
 async def start_handler(c: Client, m: Message):
     await m.reply_text(
-        text="Hi, I am UploadEver.in Uploader Bot!"
+        text="Hi, I am UploadEver.in Uploader Bot!",
         quote=True,
         reply_markup=InlineKeyboardMarkup([
         [InlineKeyboardButton("Join Channel", url="https://t.me/uploadever")]

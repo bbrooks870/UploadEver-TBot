0d01c446bfc32576216925f0ccf7c0bbb7616f39
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-18 22:28:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-class Config():
+class Config:
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")

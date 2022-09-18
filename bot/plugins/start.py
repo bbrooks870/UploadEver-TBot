ed32d5b4a6f1ed0320dc23bafb999502e67d20a3
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-18 23:04:53 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -4,6 +4,6 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start_handler(c: Client, m: Message):
-    await c.reply_message(
-        text="Hi, I am UploadEver Uploader Bot!"
+    await m.reply_message(
+        text="Hi, I am UploadEver.in Uploader Bot!"
     )

4829e5fea6362e448a0a4c36636a44dd52bbcaf3
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-18 22:26:17 +0530

    Create config.py

--- config.py
+++ config.py
@@ -20,4 +20,4 @@ class Config():
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     DIRECTORY = os.environ.get("DIRECTORY", "./downloads")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714)
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))

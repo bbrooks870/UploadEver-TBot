718763deebc4b51e280dc841942c60c2f9e10777
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-19 00:09:54 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,9 +1,13 @@
 from bot.client import Client
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start_handler(c: Client, m: Message):
     await m.reply_text(
         text="Hi, I am UploadEver.in Uploader Bot!"
+        quote=True,
+        reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton("Join Channel", url="https://t.me/uploadever")]
+        ])
     )

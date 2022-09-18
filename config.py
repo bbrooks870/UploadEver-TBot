d05dec8e6bcba1f6057574d5dbba50a33499a4b8
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-18 21:27:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %I:%M:%S %p",
+    handlers=[
+        RotatingFileHandler(
+            "log.txt", maxBytes=50000000, backupCount=10
+        ),
+        logging.StreamHandler(),
+    ],
+)
+LOGGER = logging.getLogger(__name__)
+
+class Config():
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    DIRECTORY = os.environ.get("DIRECTORY", "./downloads")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714)

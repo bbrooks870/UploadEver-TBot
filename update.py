3d8aa575223fc1e8ea76723c5bae2f0aa288c4ce
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-18 23:41:29 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,38 @@
+from os import path as opath, environ as env
+from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
+from logging.handlers import RotatingFileHandler
+from subprocess import run as srun
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %I:%M:%S %p",
+    handlers=[
+        RotatingFileHandler(
+            "log.txt", maxBytes=50000000, backupCount=10
+        ),
+        logging.StreamHandler(),
+    ],
+)
+
+UPSTREAM_REPO = env.get('UPSTREAM_REPO', "https://github.com/5MysterySD/UploadEver-TBot")
+UPSTREAM_BRANCH = env.get('UPSTREAM_BRANCH', "main")
+
+if UPSTREAM_REPO is not None:
+    if opath.exists('.git'):
+        srun(["rm", "-rf", ".git"])
+        
+    update = srun([f"git init -q \
+                     && git config --global user.email mysterysd.sd@gmail.com \
+                     && git config --global user.name tele-leechx \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+    if update.returncode == 0:
+        log_info(f'Successfully updated with latest commit from {UPSTREAM_REPO}')
+    else:
+        log_error(f'Something went wrong while updating, check {UPSTREAM_REPO} if valid or not!')

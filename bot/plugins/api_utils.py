a4de759cc5de6c2bfa42a36fcef1f62e9dce91b1
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-19 08:30:37 +0530

    Update api_utils.py

--- bot/plugins/api_utils.py
+++ bot/plugins/api_utils.py
@@ -4,9 +4,11 @@ from bot.client import Client
 from pyrogram import filters
 from pyrogram.types import Message, ForceReply
 
+USERS_API = {}
+
 @Client.on_message(filters.command("auth") & filters.private)
 async def auth_handler(c: Client, m: Message):
-    auth_msg = await m.reply_text(text="Auth Your Account to Upload Contents", reply_markup=ForceReply(True, "Enter UploadEver.in API Key"))
+    auth_msg = await m.reply_text(text="Authorization: \n1. Your Account to Upload Contents", reply_markup=ForceReply(True, "Enter UploadEver.in API Key"))
 
     input_msg: Message = await c.listen(m.chat.id)
     Token = input_msg.text
@@ -14,9 +16,12 @@ async def auth_handler(c: Client, m: Message):
         await input_msg.edit("Process Cancelled!")
         return
     else:
-        await auth_msg.delete()
         await input_msg.delete()
+        await auth_msg.delete()
         resp = rget(f"https://uploadever.in/api/account/info?key={Token}")
         jdata = resp.json()
+        USERS_API[m.chat.id] = Token
         LOGGER.info("[UploadEver.in] User Log In")
         await m.reply_text(text=f"{jdata['result']['email']} Successfully Logged In !!")
+
+

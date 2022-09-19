5a0d8e72ee925eab6290dca533e4b8fb23c05a78
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-19 21:34:14 +0530

    Update and rename api_utils.py to auth.py

--- bot/plugins/auth.py
+++ bot/plugins/auth.py
@@ -1,10 +1,8 @@
 from requests import get as rget
-from config import LOGGER
+from config import LOGGER, USERS_API
 from bot.client import Client
 from pyrogram import filters
 from pyrogram.types import Message, ForceReply
-
-USERS_API = {}
 
 @Client.on_message(filters.command("login") & filters.private)
 async def login_handler(c: Client, m: Message):
@@ -29,31 +27,6 @@ async def login_handler(c: Client, m: Message):
             LOGGER.info(f"[UploadEver.in] User: {m.chat.id} Log In Unsuccessful")
             txt = jdata['msg']
         await m.reply_text(text=txt)
-
-@Client.on_message(filters.command("stats") & filters.private)
-async def stats_handler(c: Client, m: Message):
-    Token = USERS_API.get(m.chat.id, None)
-    if Token is None:
-        text_ = "Login First /login"
-    else:
-        resp = rget(f"https://uploadever.in/api/account/info?key={Token}")
-        jdata = resp.json()
-        text_ = f'''Stats:
-
-• Email : {jdata['result']['email']}
-• Balance : {jdata['result']['balance']}
-• Storage Left : {jdata['result']['storage_left']}
-• Storage Used : {jdata['result']['storage_used'] or 0}
-• Premium Expire : {jdata['result']['premium_expire']}
-
-• Server Time : {jdata['server_time']}
-
-'''
-        resp2 = rget(f"https://uploadever.in/api/account/stats?key={Token}")
-        jdata2 = resp2.json()
-        if jdata2['msg'] != "OK" :
-            text_ += f"Account Stats :\n\n• {jdata2['msg']}"
-    await m.reply_text(text=text_)
 
 @Client.on_message(filters.command("logout") & filters.private)
 async def logout_handler(c: Client, m: Message):

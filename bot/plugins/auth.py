9a3233ef8a6f66ccc6f22b3ade304843a5145f36
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-19 23:00:03 +0530

    Update auth.py

--- bot/plugins/auth.py
+++ bot/plugins/auth.py
@@ -1,43 +1,44 @@
+from asyncio import sleep as asleep
 from requests import get as rget
 from config import LOGGER, USERS_API
 from bot.client import Client
-from pyrogram import filters
+from pyrogram import filters, enums
 from pyrogram.types import Message, ForceReply
 
 @Client.on_message(filters.command("login") & filters.private)
 async def login_handler(c: Client, m: Message):
     auth_msg = await m.reply_text(text="Authorization: \n1. Your Account to Upload Contents", reply_markup=ForceReply(True, "Enter UploadEver.in API Key"))
-
     input_msg: Message = await c.listen(m.chat.id)
     Token = input_msg.text
     if Token is None:
         await auth_msg.delete()
-        await m.reply_text("Process Cancelled!")
-        return
+        await m.reply_text("<b>👤 Login Process Cancelled!!</b>")
+        return await input_msg.continue_propagation()
     elif Token and Token.startswith("/"):
         await auth_msg.delete()
-        await m.reply_text("Process Cancelled!")
+        await m.reply_text("<b>👤 Login Process Cancelled!</b>")
         return await input_msg.continue_propagation()
     else:
         await input_msg.delete()
         await auth_msg.delete()
+        asleep(2)
         resp = rget(f"https://uploadever.in/api/account/info?key={Token}")
         jdata = resp.json()
         if jdata["status"] == 200:
             USERS_API[m.chat.id] = Token
             LOGGER.info(f"[UploadEver.in] User: {m.chat.id} Log In Success")
-            txt = f"{jdata['result']['email']} Successfully Logged In !!"
+            txt = f"<i>{jdata['result']['email']} Successfully Logged In ✅️!!</i>"
         else:
             LOGGER.info(f"[UploadEver.in] User: {m.chat.id} Log In Unsuccessful")
             txt = jdata['msg']
-        await m.reply_text(text=txt)
+        await m.reply_text(text=txt, parse_mode=enums.ParseMode.HTML, quote=True)
 
 
 @Client.on_message(filters.command("logout") & filters.private)
 async def logout_handler(c: Client, m: Message):
     try:
         USERS_API.pop(m.chat.id)
-        text_ = "Successfully LogOut"
+        text_ = "<i>🥲 You Successfully Logout.</i> <b>Do /login to Come Again<b>"
     except KeyError:
-        text_ = "Login First /login to LogOut"
-    await m.reply_text(text=text_)
+        text_ = "<b>😬 I see, you have not Login, Do <i>/login</i> to Use this Command. </b>"
+    await m.reply_text(text=text_, parse_mode=enums.ParseMode.HTML, quote=True)

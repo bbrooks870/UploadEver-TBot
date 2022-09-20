b51a0e551a54cb51fc1c58d490b8a687271b2be1
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-20 09:43:42 +0530

    Update stats.py

--- bot/plugins/stats.py
+++ bot/plugins/stats.py
@@ -1,30 +1,42 @@
+#Copyright 2022-present, Author: 5MysterySD
+
 from requests import get as rget
-from config import LOGGER, USERS_API
-from bot.client import Client
-from pyrogram import filters
+from pyrogram import filters, enums
 from pyrogram.types import Message
+
+from bot.client import Client
+from config import LOGGER, USERS_API
+from bot.core.display import convertBytes
 
 @Client.on_message(filters.command("stats") & filters.private)
 async def stats_handler(c: Client, m: Message):
+    ''' UploadEver Account Stats Fetched from API
+    :param token: Your Own API token of UploadEver.in
+
+    OUTPUT:
+    
+    '''
+
     Token = USERS_API.get(m.chat.id, None)
     if Token is None:
-        text_ = "Login First /login"
+        text_ = "<b>😬 I see, you have not Login, Do <i>/login</i> to Use this Command. </b>"
     else:
         resp = rget(f"https://uploadever.in/api/account/info?key={Token}")
         jdata = resp.json()
-        text_ = f'''Stats:
+        text_ = f'''<b>🗄 Your Account Info:</b>
 
-• Email : {jdata['result']['email']}
-• Balance : {jdata['result']['balance']}
-• Storage Left : {jdata['result']['storage_left']}
-• Storage Used : {jdata['result']['storage_used'] or 0}
-• Premium Expire : {jdata['result']['premium_expire']}
+• 📮 <b>Email :</b> <i>{jdata['result']['email']}</i>
+• 💸 <b>Balance :</b> <code>{jdata['result']['balance']}</code>
+• 📭 <b>Storage Left :</b> <code>{'Unlimited' if jdata['result']['storage_left'] == 'inf' else jdata['result']['storage_left']}</code>
+• 📬 <b>Storage Used :</b> <code>{0 if jdata['result']['storage_used'] == null else convertBytes(jdata['result']['storage_used'])}</code>
+• ⚠️ <b>Premium Expire :</b> <code>{jdata['result']['premium_expire']}</code>
 
-• Server Time : {jdata['server_time']}
+• ♻️ <b>Server Time :</b> <code>{jdata['server_time']}</code>
 
 '''
         resp2 = rget(f"https://uploadever.in/api/account/stats?key={Token}")
         jdata2 = resp2.json()
         if jdata2['msg'] != "OK" :
-            text_ += f"Account Stats :\n\n• {jdata2['msg']}"
-    await m.reply_text(text=text_)
+            text_ += f"<b>🗃 Your Account Stats :</b>\n\n• <b>{jdata2['msg']}</b>"
+
+    await m.reply_text(text=text_, parse_mode=enums.ParseMode.HTML, quote=True)

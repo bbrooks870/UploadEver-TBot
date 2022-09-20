76dc008a9d4a05079c5a7d83d8243f4bf46f7f31
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-20 09:30:57 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,13 +1,27 @@
+#Copyright 2022-present, Author: 5MysterySD
+
 from bot.client import Client
-from pyrogram import filters
+from pyrogram import filters, enums
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start_handler(c: Client, m: Message):
+    ''' Start Message of the Bot !!'''
+
     await m.reply_text(
-        text="Hi, I am UploadEver.in Uploader Bot!",
+        text='''<b>🔰 Hello, I am UploadEver.in Uploader Bot! 🔰</b>
+
+☃️  <i>Here you can Upload Files via Forwarding Telegram Files/ Sending Telegram File/Send UploadEver Links/ Direct Download Links to this Bot!</i>
+
+<u>Look Like you haven't logged in yet hit /login to logged in and Use Bot Features!</u>
+<code>Want to know bot features hit /help</code>
+
+<b>💯 Bot Created By ♥️ @MysterySD ♥️ & Powered By ⚡️ UploadEver ⚡️</b>
+<i>If you face any kind of Problem/Error or Feature Request, Message to @MysterySD</i>''',
         quote=True,
+        parse_mode=enums.ParseMode.HTML,
         reply_markup=InlineKeyboardMarkup([
-        [InlineKeyboardButton("Join Channel", url="https://t.me/uploadever")]
+        [InlineKeyboardButton("🗓 Join Channel", url="https://t.me/uploadever"),
+        InlineKeyboardButton("📇 Website", url="https://uploadever.in")]
         ])
     )

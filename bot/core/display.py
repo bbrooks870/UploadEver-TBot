4767454e37606b3008f9a918904505eb06849b55
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-09-20 10:03:57 +0530

    Create display.py

---
+++ bot/core/display.py
@@ -0,0 +1,23 @@
+#Copyright 2022-present, Author: 5MysterySD
+
+def convertBytes(sz) -> str:
+    if not sz: return ""
+    ind = 0
+    Units = {0: 'Bytes', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB', 5: 'PB', 6: 'EB'}
+    while sz > 2**10:
+        sz /= 2**10
+        ind += 1
+    return f"{round(sz, 2))} {Units[ind]}"
+
+
+def convertTime(mss: int) -> str:
+    s, ms = divmod(int(mss), 1000)
+    m, s = divmod(s, 60)
+    hr, m = divmod(m, 60)
+    days, hr = divmod(hr, 24)
+    convertedTime = ((str(days) + " days, ") if days else "") + \
+          ((str(hr) + " hours, ") if hr else "") + \
+          ((str(m) + " min, ") if m else "") + \
+          ((str(s) + " sec, ") if s else "") + \
+          ((str(ms) + " msec, ") if ms else "")
+    return convertedTime[:-2]
